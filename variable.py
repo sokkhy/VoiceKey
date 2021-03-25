4bdc5e8eca971f7c25d61541011e030d3b11cc8a
sokkhy <khysok95@gmail.com>
2021-03-25 14:12:29 +0700

    voice.py

--- variable.py
+++ variable.py
@@ -1,18 +1,33 @@
-counter = 100          # An integer assignment
-miles   = 1000.0       # A floating point
-name    = "John"       # A string
-list    = ['1','2']    # A list
-print ("counter " , str(counter))
-print ("miles ", int(miles))
 
-print ("list ", list)
+# counter = 100          # An integer assignment
+# miles   = 1000.0       # A floating point
+# name    = "John"       # A string
+# list    = ['1','2']    # A list
 
-res = name.strip('][').split(', ') 
+# String_to_List  = name.strip('][').split(', ') 
+# List_to_String  = str(list)
+# Int_to_String   = str(counter)
+# Float_to_Int    = int(miles)
 
-print ("name ", res)
+# print ("counter " , type(Int_to_String))         # number to string 
+# print ("miles ",    type(Float_to_Int))           # float to int 
+# print ("String_To_List ",  type(String_to_List)) # String to List
+# print ("List_To_String ",  type(List_to_String)) # List to String
 
 
-__castList__= str(list)
-print ("list ", __castList__)
 
-# for i in x:
+
+
+
+
+c = 1 + 2j
+#Real Part of complex number
+print('Complex Number: Real Part is = ', c. real)
+
+
+#Imaginary Part of complex number
+print('Complex Number: Imaginary Part is = ', c. imag)
+
+
+##Conjugate of complex number
+print('Complex Number: conjugate Part = ', c. conjugate())
--- voice.py
+++ voice.py
@@ -21,23 +21,7 @@ CHOICE = [
     'DAVID',
     'KEY'
 ]
-class SpeechRecognizer(threading.Thread):
-    
-    def __init__(self):
-        super(SpeechRecognizer, self).__init__()
-        self.setDaemon(True)
-        self.recognized_text = "initial"
 
-    def run(self):
-        while True:
-            print("thread call: ")
-    
-            time.sleep(1.0)
-            # self.recognized_text = random.choice(self.ANSWERS)
-
-
-# recognizer = SpeechRecognizer()
-# recognizer.start()
 def talk():
     input=sr.Recognizer()
     with sr.Microphone() as source:
@@ -61,15 +45,6 @@ def respond(output):
     response.save(file)
     playsound.playsound(file, True)
     os.remove(file)
-    def __init__(self, master,root):
-        self.root = root
-        frame = tkinter.Frame(master)
-        frame.pack()
-        image = Image.open("ball.jpg")
-        photo = ImageTk.PhotoImage(image)
-    def get_answer (self):
-        self.answer_text.set(random.choice(CHOICE))
-        root.after(100, self.update_recognized_text)
 
 class App(object):
     def __init__(self, master):
@@ -84,75 +59,75 @@ class App(object):
         self.crystal_ball.pack()
         self.answer.place(relx = 0.5, rely = 0.4, anchor=tkinter.CENTER)
         self.quit_button = tkinter.Button(frame, text = "QUIT", command = frame.quit)
-        self.ask_button = tkinter.Button(frame, text = "ASk", command = random.choice(CHOICE))
+        self.ask_button = tkinter.Button(frame, text = "ASk", command = self.start)
         self.ask_button.pack(side = tkinter.LEFT)
                     
     def get_answer (self):
         self.answer_text.set(random.choice(CHOICE))
 
-
-# root = tkinter.Tk()
-# app = App(root)
-# root.mainloop()
-if __name__=='__main__':
-
-    respond("Hi, I am Pike. I will assist you Sir!")
-        
-    while(1):
-
-        respond("Let's make a command Sir!")
-        text=talk().lower()
-        if text==0:
-            continue
-        if "who" in str(text):
-            respond(random.choice(CHOICE))
-        if "stop" in str(text) or "exit" in str(text) or "bye" in str(text):
-            respond("Ok bye and take care")
-            break
-            
-        if 'wikipedia' in text:
-            respond('Searching Wikipedia')
-            text =text.replace("wikipedia", "")
-            results = wikipedia.summary(text, sentences=3)
-            respond("According to Wikipedia")
-            print(results)
-            respond(results)
-                  
-        elif 'time' in text:
-            strTime=datetime.datetime.now().strftime("%H:%M:%S")
-            respond(f"the time is {strTime}")     
-        elif 'search'  in text:
-            text = text.replace("search", "")
-            webbrowser.open_new_tab(text)
-            time.sleep(5)
-        
-        elif "calculate" or "what is" in text: 
-            question=talk()
-            print("testss " + text)
-            app_id= "H547RP-7YTRPJ77UA"
-            client = wolframalpha.Client(app_id)
-            res = client.query(text)
-            answer = next(res.results).text
-            print("tets  "+ answer)
-            respond("The answer is " + answer)
-            
-        elif 'open google' in text:
-            webbrowser.open_new_tab("https://www.google.com")
-            respond("Google is open")
-            time.sleep(5)
-            
-        elif 'youtube' in text: 
-            driver = webdriver.Chrome(r"Mention your webdriver location") 
-            driver.implicitly_wait(1) 
-            driver.maximize_window()
-            respond("Opening in youtube") 
-            indx = text.split().index('youtube') 
-            query = text.split()[indx + 1:] 
-            driver.get("http://www.youtube.com/results?search_query =" + '+'.join(query))              
+    def start (self):
+    
+            respond("Hi, I am Pike. I will assist you Sir!")
+            while(1):
+                respond("Let's make a command Sir!")
+                text=talk().lower()
+                if text==0:
+                    continue
+                if "who" in str(text):
+                    respond(random.choice(CHOICE))
+                if "stop" in str(text) or "exit" in str(text) or "bye" in str(text):
+                    respond("Ok bye and take care")
+                    break
+                    
+                if 'wikipedia' in text:
+                    respond('Searching Wikipedia')
+                    text =text.replace("wikipedia", "")
+                    results = wikipedia.summary(text, sentences=3)
+                    respond("According to Wikipedia")
+                    print(results)
+                    respond(results)
+                        
+                elif 'time' in text:
+                    strTime=datetime.datetime.now().strftime("%H:%M:%S")
+                    respond(f"the time is {strTime}")     
+                elif 'search'  in text:
+                    text = text.replace("search", "")
+                    webbrowser.open_new_tab(text)
+                    time.sleep(5)
                 
-        elif "open word" in text: 
-            respond("Opening Microsoft Word") 
-            os.startfile('Mention location of Word in your system') 
-        
-        else:
-           respond("Application not available")
+                elif "calculate" or "what is" in text: 
+                    question=talk()
+                    print("testss " + text)
+                    app_id= "H547RP-7YTRPJ77UA"
+                    client = wolframalpha.Client(app_id)
+                    res = client.query(text)
+                    answer = next(res.results).text
+                    print("tets  "+ answer)
+                    respond("The answer is " + answer)
+                    
+                elif 'open google' in text:
+                    webbrowser.open_new_tab("https://www.google.com")
+                    respond("Google is open")
+                    time.sleep(5)
+                    
+                elif 'youtube' in text: 
+                    driver = webdriver.Chrome(r"Mention your webdriver location") 
+                    driver.implicitly_wait(1)
+                    driver.maximize_window()
+                    respond("Opening in youtube") 
+                    indx = text.split().index('youtube') 
+                    query = text.split()[indx + 1:] 
+                    driver.get("http://www.youtube.com/results?search_query =" + '+'.join(query))              
+                        
+                elif "open word" in text: 
+                    respond("Opening Microsoft Word") 
+                    os.startfile('Mention location of Word in your system') 
+                
+                else:
+                    respond("Application not available")
+
+root = tkinter.Tk()
+app = App(root)
+root.mainloop()
+
+    
